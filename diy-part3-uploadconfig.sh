e31487608c9b8ceffa292f826ec8169bf9031e6f
iamaluckyguy <64719828+iamaluckyguy@users.noreply.github.com>
2020-05-10 13:30:08 +0800

    Create diy-part3-uploadconfig.sh

---
+++ diy-part3-uploadconfig.sh
@@ -0,0 +1,24 @@
+  
+#!/bin/sh
+
+#upload config to ftp after menuconfig
+
+./scripts/diffconfig.sh > seed.config
+
+cd openwrt
+date=`date +%Y-%m-%d-%H-%M-%S`
+newfilename=seed.config-$date
+
+cp seed.config $newfilename
+
+PUTFILE=$newfilename
+ftp -v -n 34.92.60.168<<EOF
+user yourchinaoffice yourchinaofficeftp
+passive
+binary
+prompt
+put $PUTFILE
+bye
+#here document
+EOF
+echo "commit to ftp successfully"
